2f3c6ad01beaf0e9883f8e901b8d54522856ac89
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-06 09:50:27 +0100

    Update Banner.jsx

--- dre-commerce/src/components/Banner.jsx
+++ dre-commerce/src/components/Banner.jsx
@@ -5,18 +5,20 @@ import {BsFillArrowLeftSquareFill,BsFillArrowRightSquareFill  } from 'react-icon
 const Banner = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   
-  const nextSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? 1 : 0);
+  const prevSlide = () => {
+    setCurrentSlide((currentSlide - 1 + 2) % 2);//(currentSlide - 1 + 2) % 2 to go back to the previous slide.
   };
 
-  const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? 1 : 0);
+  const nextSlide = () => {
+    setCurrentSlide((currentSlide + 1) % 2); //(currentSlide + 1) % 2 to advance to the next slide
   };
+
+
 
   return (
     <div className="w-full h-auto overflow-x-hidden">
       <div className="w-screen h-[500px] relative ">
-        <div className="w-[400vw] h-full flex" style={{transform: `translateX(-${currentSlide*100}vw)`}}>
+        <div className="w-[400vw] h-full flex transition-transform duration-1000" style={{transform: `translateX(-${currentSlide*100}vw)`}}>
           <img
             className="w-screen h-full object-cover"
             src={BannerOne}
@@ -31,11 +33,11 @@ const Banner = () => {
           />
         </div>
         <div onClick={prevSlide} className='absolute w-fit left-0 right-0 mx-auto flex gap-8 bottom-10'>
-          <div  className='text-2xl text-blue-300 hover:cursor-pointer hover:text-gray-700
+          <div  className='text-2xl text-gray-700 hover:cursor-pointer hover:text-gray-300
           active:text-blue-500 duration-300'>
             <BsFillArrowLeftSquareFill />
           </div>
-          <div onClick={nextSlide}  className='text-2xl text-blue-300 hover:cursor-pointer hover:text-gray-700 
+          <div onClick={nextSlide}  className='text-2xl text-gray-700 hover:cursor-pointer hover:text-gray-300 
           active:text-blue-500 duration-300'>
             <BsFillArrowRightSquareFill />
           </div>
