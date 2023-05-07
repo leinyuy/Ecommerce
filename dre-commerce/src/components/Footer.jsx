09f93386ff13eb2ddddc5a58f3250f6357506fcc
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-07 15:49:16 +0100

    Update Footer.jsx

--- dre-commerce/src/components/Footer.jsx
+++ dre-commerce/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, Fahome, } from 'react-i
 
 const Footer = () => {
   return (
-    <div className='bg-black tex-[#949494] py-20 font-titleFont'>
+    <div className='bg-black text-[#949494] py-20 font-titleFont'>
           <div className='max-w-screen-xl mx-auto'>
               <div className='flex flex-col gap-7'>
                   <img className='w-32' src={logoLight} alt="logoLight" />
