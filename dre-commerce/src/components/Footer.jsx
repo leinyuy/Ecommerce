17db545a4a3f14ac94b149da95f9c0a6ad26894d
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-08 15:27:46 +0100

    Update Footer.jsx

--- dre-commerce/src/components/Footer.jsx
+++ dre-commerce/src/components/Footer.jsx
@@ -6,9 +6,9 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, Fahome, } from 'react-i
 const Footer = () => {
   return (
     <div className='bg-black text-[#949494] py-20 font-titleFont'>
-          <div className='max-w-screen-xl mx-auto'>
+          <div className='max-w-screen-xl mx-auto grid grid-cols-4'>
               <div className='flex flex-col gap-7'>
-                  <img className='w-32' src={logoLight} alt="logoLight" />
+                  <img className='w-32 h-7' src={logoLight} alt="logoLight" />
                   <img className='w-32' src={paymentLogo} alt="payment logo" />
                   <div className='flex gap-5 text-lg text-gray-400'>
                       <FaYoutube className='hover:text-white duration-300 cursor-pointer' />
@@ -17,6 +17,7 @@ const Footer = () => {
                       <FaInstagram className='hover:text-white duration-300 cursor-pointer'/>
                   </div>
               </div>
+              <h2 className='text-2xl font-semibold text-white mb-4'>Locate us</h2>
       </div>
     </div>
   )
