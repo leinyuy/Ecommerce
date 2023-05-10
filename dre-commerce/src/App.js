94ee9411f4815ddba3a73e23b5b339450eba36a9
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-10 18:52:38 +0100

    setting up react router dom

--- dre-commerce/src/App.js
+++ dre-commerce/src/App.js
@@ -2,14 +2,47 @@ import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Cart from "./pages/Cart";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from 'react-router-dom';
+
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/cart",
+        element: <Cart/>,
+      }
+    ]
+  }
+])
 
 
 function App() {
   return (
     <div className = "font-bodyFont">
-      <Header />
-      <Home />
-      <Footer/>
+      <RouterProvider router={ router } />
     </div>
   );
 }
