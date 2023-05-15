5279b3762fc7f2899c23562f791e42ca1b8be811
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-05-15 21:39:31 +0100

    Update ProductsCard.jsx

--- dre-commerce/src/components/ProductsCard.jsx
+++ dre-commerce/src/components/ProductsCard.jsx
@@ -4,23 +4,38 @@ import React from 'react'
 const ProductsCard = ({ product }) => {
   console.log(product)
   return (
-    <div className='group'>
-      <div className='w-full h-96 cursor-pointer overflow-hidden'>
-        <img className='w-full object-cover group-hover:scale-110 duration-500' src={product.image} alt="productImg" />
+    <div className="group">
+      <div className="w-full h-96 cursor-pointer overflow-hidden">
+        <img
+          className="w-full object-cover group-hover:scale-110 duration-500"
+          src={product.image}
+          alt="productImg"
+        />
       </div>
-      <div className='w-full border-[1px] px-2 py-4'>
-        <div>
-          <div className='flex justify-be
-          '>
-            <h2 className='font-titleFont text-base font-bold'>{product.title}</h2> 
-            <div>
-              <p>${ product.price}</p>
+      <div className="w-full border-[1px] px-2 py-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="font-titleFont text-base font-bold">
+              {product.title.substring(0, 30)}
+            </h2>
+          </div>
+          <div className="flex gap-2 relative overflow-hidden w-28 text-sm">
+            <div className=" gap-2 transform group-hover:translate-x-24 transition-transform duration-500">
+              <p className="text-gray-500 mb-6">${product.price}</p>
             </div>
+            <p
+              className="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900
+            flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0
+             transition-transform cursor-pointer duration-500"
+            >
+              
+              add to cart
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProductsCard
