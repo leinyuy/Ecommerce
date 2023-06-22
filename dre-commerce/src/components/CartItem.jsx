5f62e3551df95f973c3586f1212afa70e35b7dbf
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-06-22 12:03:43 +0100

    finishing the cart page and setting up the persistStore
    this is so that the page doesn't clear the cart upon refresh. so the items are stored in the redux

--- dre-commerce/src/components/CartItem.jsx
+++ dre-commerce/src/components/CartItem.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
+import {Link} from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { MdOutlineClose } from 'react-icons/md';
 import { HiOutlineArrowLeft } from 'react-icons/hi';
-import { deleteItem } from '../redux/cartlySlice';
+import { decreamentQuantity,increamentQuantity, deleteItem, resetCart } from '../redux/cartlySlice';
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 
@@ -41,10 +42,35 @@ export const CartItem = () => {
                              <div className='w-52 flex items-center justify-between text-gray-500 gap-4 border p-3'>
                             <p className='text-sm'>Quantity</p>
                             <div className='flex items-center gap-4 text-sm font-semibold'>
-                                <span className='border h-5 font-normal text-lg flex items-center justify-center
+
+                                <span 
+                                    onClick={()=>
+                                    dispatch(
+                                        decreamentQuantity({
+                                            id: item.id,
+                                            title: item.title,
+                                            image: item.image,
+                                            price: item.price,
+                                            quantity: 1,
+                                            description: item.description,
+                                        })
+                                    )}
+                                className='border h-5 font-normal text-lg flex items-center justify-center
            px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black'>-</span>
-                                <span>1</span> 
-                                <span className='border h-5 font-normal text-lg flex items-center justify-center
+                                <span>{item.quantity}</span> 
+                                <span
+                                    onClick={()=>
+                                        dispatch(
+                                            increamentQuantity({
+                                                id: item.id,
+                                                title: item.title,
+                                                image: item.image,
+                                                price: item.price,
+                                                quantity: 1,
+                                                description: item.description,
+                                            })
+                                        )}
+                                className='border h-5 font-normal text-lg flex items-center justify-center
            px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black'>
                                     +
                                 </span>
@@ -56,7 +82,16 @@ export const CartItem = () => {
                     ))
                 }
             </div>
-            <ToastContainer
+            <button onClick={()=>dispatch(resetCart()) & toast.error('Your Cart is Empty!')} className='bg-red-500 text-white mt-8 ml-7 py-1 px-6 hover:bg-red-800 duration-300'>Reset Cart</button>
+           <Link to ='/'>
+           
+           <button className='mt-8 ml-7 flex items-center gap-1 text-gray-400 hover:text-black duration-300'>
+           <span>
+                <HiOutlineArrowLeft/>
+            </span>
+            Go Shopping</button>
+           </Link>
+           <ToastContainer
           position="top-left"
           autoClose={2000}
           hideProgressBar={false}
--- dre-commerce/src/redux/store.js
+++ dre-commerce/src/redux/store.js
@@ -1,8 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit'
+
+import{
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import cartlyReducer from './cartlySlice';
 
-export default configureStore({
-  reducer: {
-    cartly: cartlyReducer,
-  },
-})
+const persistConfig={
+  key:"root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, cartlyReducer); 
+
+
+
+export const store = configureStore({
+  reducer: {cartly: persistedReducer},
+  middleware: (getDefaultMiddleware)=>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+
+});
+
+export let persistor = persistStore(store);
